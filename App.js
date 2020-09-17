068cb07f630f65bf811f2e4b8cc4a6dc7c2b0cbc
CameronK24 <cskelly15@gmail.com>
2020-09-17 11:54:14 -0600

    added testing for router

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Router, Scene} from 'react-native-router-flux';
+import TestRouting from './Components/TestRouting';
+import TestRouting2 from './Components/TestRouting2';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Router>
+      <Scene key='root'>
+        <Scene key='test1'
+          component={TestRouting}
+          title='test1'
+          initial  
+        />
+        <Scene key='test2'
+          component={TestRouting2}
+          title='test2' 
+        />
+      </Scene>
+    </Router>
   );
 }
 
