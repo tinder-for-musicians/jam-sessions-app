b4ee6914d7b7d3e492fa154505a97f7eaab4da88
CameronK24 <cskelly15@gmail.com>
2020-10-19 15:22:09 -0600

    started work on landing page

--- App.js
+++ App.js
@@ -4,15 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import {Router, Scene} from 'react-native-router-flux';
 import TestRouting from './Components/TestRouting';
 import TestRouting2 from './Components/TestRouting2';
+import Landing from './Components/Landing/Landing';
 
 export default function App() {
   return (
     <Router>
       <Scene key='root'>
+        <Scene key='loginPage'
+          component={Landing}
+          title='loginPage'
+          initial
+        />
         <Scene key='test1'
           component={TestRouting}
-          title='test1'
-          initial  
+          title='test1'  
         />
         <Scene key='test2'
           component={TestRouting2}
--- Components/Landing/Landing.js
+++ Components/Landing/Landing.js
@@ -6,14 +6,20 @@ import {
 } from 'react-native';
 import {Actions} from 'react-native-router-flux';
 
-
-const TestRouting2 = () => {
+const Landing = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}
-      onPress={() => Actions.loginPage()}>
-        Gray Screen
-      </Text>
+      {/* <Text style={styles.welcome}
+        onPress={() => Actions.test1()}>
+        Landing Page
+      </Text> */}
+      <form className='landing-view'>
+        <section className='auth-container'>
+            <h1 className='logo'>
+                Jam Sessions
+            </h1>
+        </section>
+      </form>
     </View>
   );
 }
@@ -23,7 +29,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#a3a3a3',
+    backgroundColor: '#bb0000',
   },
   welcome: {
     fontSize: 20,
@@ -33,4 +39,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default TestRouting2;
+export default Landing;
